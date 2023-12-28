a5b3a7f60cae6991a08feabef4c51f798f187833
Luan Pablo <luanpabloj.1@gmail.com>
2023-12-27 23:14:30 -0300

    restaurando arquivos

---
+++ Primeiro semestre/Algoritmos e técnicas de programação/atividade switchcase/Atividade2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Atividade2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor inteiro");
+            int valor = int.Parse(Console.ReadLine());
+
+            switch (valor)
+            {
+                case 0:
+                Console.WriteLine("Vermelho");
+                break;
+
+                case 1:
+                Console.WriteLine("Branco");
+                break;
+
+                case 2:
+                Console.WriteLine("Preto");
+                break;
+
+
+
+                default:
+                Console.WriteLine("Valor não encontrado no banco de dados");
+                break;
+            }
+
+            Console.WriteLine("Luan Pablo Mendes Amaral");
+
+
+        }
+    }
+}
---
+++ Primeiro semestre/Algoritmos e técnicas de programação/dowhile5/questao3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace questao3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num=0, cont=1, soma=0;
+            double quadrado=0, cubo=0, raiz=0;
+
+            do{
+                Console.WriteLine("Digite o valor do produto: ");
+                num = int.Parse(Console.ReadLine());
+
+                soma+= num;
+                quadrado = Math.Pow(soma, 2);
+                cubo = Math.Pow(soma, 3);
+                raiz = Math.Sqrt(soma);
+
+                if(cont % 20 == 0){
+                    Console.WriteLine("soma dos valores digitados: " + soma);
+                    Console.WriteLine("valor ao quadrado dos valores digitados: " + quadrado);
+                    Console.WriteLine("valor ao cubo dos valores digitados: " + cubo);
+                    Console.WriteLine("raiz dos valores digitados: " + raiz);
+
+
+
+                }
+
+                if(num == 0 || num < 0){
+                    break;
+                }
+
+                cont++;
+            }
+            while(true);
+
+
+
+
+        }
+    }
+}
